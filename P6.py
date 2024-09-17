0de56b84c8a8e34d9d920870ff2291943917be7d
JonathanG12 <65064232+JonathanG12@users.noreply.github.com>
2024-09-17 15:54:43 -0600

    ..

--- P6.py
+++ P6.py
@@ -1,19 +1,36 @@
-import cv2  ##opencv
-cam = cv2.VideoCapture(0) ##videocamara ---
-contFotos = 0
-while True:
-    result, image = cam.read()
+# pylint: disable=no-member
+
+import cv2
+import os
+
+# Crear la carpeta "1" si no existe
+folder_name = "5"
+if not os.path.exists(folder_name):
+    os.makedirs(folder_name)
+
+# Iniciar la videocámara0
+cam = cv2.VideoCapture(0)  # videcámara (0 para la cámara predeterminada)
+
+# Contador de fotos00
+contFotos = 00
+totalFotos = 175  # Número total de fotos que deseas capturar
+
+while contFotos < totalFotos:
+    result, image = cam.read()  # Captura el frame
     if result:
-        cv2.imshow("Camara_Principal", image)
-        res = cv2.waitKey(1) ## 1  = .. no detenga la ejecucion
-        #print(res , "  ", ord("q"))
-        if res == ord("q"):
-            cam.release()
-            cv2.destroyWindow("Camara_Principal")
-            break
-        elif res == ord(" "): ##space
-            cv2.imwrite("foto_"+ str(contFotos) +".png", image)
-            contFotos+=1
+        cv2.imshow("Camara_Principal", image)  # Muestra el video en una ventana
+
+        # Guardar la imagen en la carpeta "1"
+        file_name = os.path.join(folder_name, f"foto_{contFotos}.png")
+        cv2.imwrite(file_name, image)
+        contFotos += 1
+
+        # Esperar brevemente entre fotos (ajusta si es necesario)
+        cv2.waitKey(100)  # 100 milisegundos entre cada captura
     else:
-        print("No image detected. Please! try again")
-        break
+        print("No image detected. Please try again")
+        break
+
+# Liberar la cámara y cerrar la ventana
+cam.release()
+cv2.destroyAllWindows()
